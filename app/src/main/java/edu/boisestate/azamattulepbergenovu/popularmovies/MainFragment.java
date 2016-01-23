a0b4b2abb64fb6ee03305db5e7b06207e62749ee
atulep <aza.tulepber@gmail.com>
2016-01-23 16:28:50 -0700

    Added basic method signatures to Fragment class.

---
+++ app/src/main/java/edu/boisestate/azamattulepbergenovu/popularmovies/MainFragment.java
@@ -0,0 +1,34 @@
+package edu.boisestate.azamattulepbergenovu.popularmovies;
+
+/* My understanding is that since my minimum SDK is IceCreamSandwich, I am safe to use
+android.app.Fragment instead android.support.v4.fragment.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by atulep on 1/23/2016.
+ */
+public class MainFragment extends Fragment {
+
+    /**
+     * Pubic no-argument constructor.
+     */
+    public MainFragment () {
+
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+    }
+
+}
