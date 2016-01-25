0299459d0854328aab508353879ef144d18476e5
atulep <aza.tulepber@gmail.com>
2016-01-24 17:01:00 -0700

    Added code to onCreateView of MainFragment and eliminated any flagger errors.

--- app/src/main/java/edu/boisestate/azamattulepbergenovu/popularmovies/MainFragment.java
+++ app/src/main/java/edu/boisestate/azamattulepbergenovu/popularmovies/MainFragment.java
@@ -9,11 +9,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.Arrays;
 
 /**
  * Created by atulep on 1/23/2016.
  */
 public class MainFragment extends Fragment {
+
+    //Hard coded list of movies.
+    Movie[] list = {
+      new Movie()
+    };
 
     /**
      * Pubic no-argument constructor.
@@ -34,6 +42,16 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         //TODO: Need to create create an AdapterView -- GridView. Bind the MoviePosterAdapter to it.
         //Return the resulted view.
+        //Need to check if savedInstanceState exists, if so then inflate it.
+        //Otherwise, need to modify container to be a GridView.
+        //Create an adapter to bind it a gridview.
+        //Get a view from a GridView and return it.
+
+        View rootView = inflater.inflate(R.layout.activity_main, container, false);
+        MoviePosterAdapter adapter = new MoviePosterAdapter(getActivity(), Arrays.asList(list));
+        GridView grid = (GridView) rootView.findViewById(R.id.gridView_main);
+        grid.setAdapter(adapter);
+        return rootView;
     }
 
 }
