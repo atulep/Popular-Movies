27c601a52b4b31664b2098c85332461ae2a6ce57
atulep <aza.tulepber@gmail.com>
2016-01-27 21:07:40 -0700

    Added an API key to a secure place. Added code for Networking call.

--- app/src/main/java/edu/boisestate/azamattulepbergenovu/popularmovies/MainFragment.java
+++ app/src/main/java/edu/boisestate/azamattulepbergenovu/popularmovies/MainFragment.java
@@ -5,12 +5,21 @@ android.app.Fragment instead android.support.v4.fragment.
  */
 
 import android.app.Fragment;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 
 /**
@@ -60,8 +69,92 @@ public class MainFragment extends Fragment {
         MoviePosterAdapter adapter = new MoviePosterAdapter(getActivity(), Arrays.asList(list));
         GridView grid = (GridView) rootView.findViewById(R.id.gridView_main);
         grid.setAdapter(adapter);
-
         return rootView;
     }
 
+    public class FetchMovieDataTask extends AsyncTask<Void, Void, Void> {
+        private String LOG_TAG = this.getClass().getSimpleName();
+
+        public Void doInBackground(Void ... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String typeOfSort = params[0];// which sort to perform
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+            String[] simplifiedForecast = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String FORECAST_BASE_URL =
+                        "http://api.themoviedb.org/3/discover/movie?";
+                final String QUERY_PARAM = "sort_by=";
+                final String APPID_PARAM = "APPID";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, typeOfSort)
+                        .appendQueryParameter(APPID_PARAM, BuildConfig.MOVIE_DB_API_KEY)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "JSON Parsed String:" + forecastJsonStr);
+
+                try {
+                    simplifiedForecast = getWeatherDataFromJson(forecastJsonStr, 7);
+                } catch (org.json.JSONException e) {
+                    Log.e(LOG_TAG, "ERROR with fetching the simpliged forecast.");
+                    System.exit(1);
+                }
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                forecastJsonStr = null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return simplifiedForecast;
+        }
+
+        }
+    }
 }
