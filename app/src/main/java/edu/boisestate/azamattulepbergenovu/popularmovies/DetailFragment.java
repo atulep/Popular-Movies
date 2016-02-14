98d946742e8adeafa9720083c8be74519040fae1
atulep <aza.tulepber@gmail.com>
2016-02-14 02:15:11 -0700

    Added ButterKnife into my code.

--- app/src/main/java/edu/boisestate/azamattulepbergenovu/popularmovies/DetailFragment.java
+++ app/src/main/java/edu/boisestate/azamattulepbergenovu/popularmovies/DetailFragment.java
@@ -10,12 +10,21 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Displays details of the movie.
  * Created by atulep on 2/3/2016.
  */
 public class DetailFragment extends Fragment {
     private String LOG_TAG = getClass().getSimpleName();
+    @Bind(R.id.details_imageView) ImageView poster;
+    @Bind(R.id.details_title) TextView title;
+    @Bind(R.id.details_release) TextView release;
+    @Bind(R.id.details_rating) TextView rating;
+    @Bind(R.id.details_plot) TextView plot;
+
 
     public void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
@@ -23,25 +32,17 @@ public class DetailFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, rootView);
         Movie obj = (Movie) getActivity().getIntent().getParcelableExtra(getResources().getString(R.string.parcelable_movie_key));
         updateMovieDetails(rootView, obj);
         return rootView;
     }
 
     public void updateMovieDetails(View view, Movie movie){
-        ImageView poster = (ImageView) view.findViewById(R.id.details_imageView);
         Picasso.with(this.getActivity()).load("http://image.tmdb.org/t/p/w185" + movie.posterImage).into(poster);
-
-        TextView title = (TextView) view.findViewById(R.id.details_title);
         title.setText(movie.title);
-
-        TextView release = (TextView) view.findViewById(R.id.details_release);
         release.setText(movie.releaseDate);
-
-        TextView rating = (TextView) view.findViewById(R.id.details_rating);
         rating.setText(Double.valueOf(movie.rating).toString());
-
-        TextView plot = (TextView) view.findViewById(R.id.details_plot);
         plot.setText(movie.plot);
     }
 }
