726743868058a54703f098de211a0a97163ab05f
atulep <aza.tulepber@gmail.com>
2016-07-10 20:03:10 -0600

    Added Schematic dependency into the Gradle files. Added columns for the movies, trailers, and reviews table.

---
+++ app/src/main/java/edu/boisestate/azamattulepbergenovu/popularmovies/Data/MoviesContract.java
@@ -0,0 +1,52 @@
+package edu.boisestate.azamattulepbergenovu.popularmovies.Data;
+
+import net.simonvt.schematic.annotation.AutoIncrement;
+import net.simonvt.schematic.annotation.DataType;
+import net.simonvt.schematic.annotation.NotNull;
+import net.simonvt.schematic.annotation.PrimaryKey;
+
+/**
+ * Created by atulep on 7/10/2016.
+ */
+public class MoviesContract {
+
+    /**
+     * Columns for the table containing general movie details used in the detail fragment.
+     */
+    public interface DetailsColumns {
+        @DataType(DataType.Type.INTEGER) @PrimaryKey @AutoIncrement String _ID = "_id";
+
+        @DataType(DataType.Type.INTEGER) @NotNull String MOVIE_ID = "movie_id";
+
+        @DataType(DataType.Type.TEXT) @NotNull String TITLE = "title";
+
+        @DataType(DataType.Type.TEXT) @NotNull String POSTER_IMAGE = "poster_image";
+
+        @DataType(DataType.Type.TEXT) @NotNull String PLOT = "plot";
+
+        @DataType(DataType.Type.REAL) @NotNull String RATING = "rating";
+
+        @DataType(DataType.Type.TEXT) @NotNull String RELEASE_DATE = "release_data";
+    }
+
+    /**
+     * Columns for the table containing the trailer information for the movie_id
+     */
+    public interface TrailerColumns {
+        @DataType(DataType.Type.INTEGER) @PrimaryKey @AutoIncrement String _ID = "_id";
+
+        @DataType(DataType.Type.INTEGER) @NotNull String MOVIE_ID = "movie_id";
+
+        // some movies may not have trailers
+        @DataType(DataType.Type.TEXT) String TRAILER_PATH = "trailer_path";
+    }
+
+    public interface ReviewColumns {
+        @DataType(DataType.Type.INTEGER) @PrimaryKey @AutoIncrement String _ID = "_id";
+
+        @DataType(DataType.Type.INTEGER) @NotNull String MOVIE_ID = "movie_id";
+
+        // some movies may not have reviews
+        @DataType(DataType.Type.TEXT) String REVIEW = "review";
+    }
+}
